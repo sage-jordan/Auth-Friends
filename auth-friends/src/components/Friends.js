0b07b8e0a3d062ffd32305eccb2d8aab358562b3
sage-jordan <sagemjrodan@gmail.com>
2019-10-04 02:15:12 -0400

    got cards to show and friends data to call

--- auth-friends/src/components/Friends.js
+++ auth-friends/src/components/Friends.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card } from 'semantic-ui-react';
+import { axiosWithAuth } from '../auth/axiosWithAuth';
 
 function Friends(){
+    const [ friendsObj, setFriendsObj ] = useState([]);
+
+    useEffect(() => {
+        axiosWithAuth()
+            .get('/friends')
+            .then(res => {
+                return setFriendsObj(res.data);
+            })
+            .catch(err => console.log('Error: ', err));
+    }, [] );
+    console.log(friendsObj);
     return (
         <div>
             <h1>Friends List</h1>
+            {friendsObj.map((friend) => {
+                return (
+                    <div className="friend-cards">
+                        <Card
+                            header={friend.name}
+                            meta={friend.age}
+                            description={friend.email}
+                            extra={friend.id}
+                        />
+                    </div>
+                );
+            })}
         </div>
     );
 };
