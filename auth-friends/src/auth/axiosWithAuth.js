7a6119b997a3579d4d436ae75e8f9b7ace4ae39e
sage-jordan <sagemjrodan@gmail.com>
2019-10-04 01:31:29 -0400

    fixed axios call

--- auth-friends/src/auth/axiosWithAuth.js
+++ auth-friends/src/auth/axiosWithAuth.js
@@ -1,6 +1,8 @@
+import axios from 'axios';
+
 export const axiosWithAuth = () => {
     const token = localStorage.getItem('token');
-    return axiosWithAuth.create({
+    return axios.create({
         baseURL: 'http://localhost:5000/api',
         headers: {
             Authorization: token
